3d812369bfa48251b78e15135390f5f446e6d5b2
Mahesh Sadupalli <132281261+mahesh-sadupalli@users.noreply.github.com>
2024-03-05 21:49:08 +0100

    Delete Concatination.py

--- Concatination.py
+++
@@ -1,9 +0,0 @@
-#Write a function named right_justify that takes a string named s as a parameter. Using string concatenation and repetition 
-
-def right_justify(s):
-    spaces = " "*(70-len(s)) #70 spaces and substracting of string length 
-    justify_right= spaces + s #add the string length 
-    print(justify_right)
-
-right_justify('Samantha')
-            
